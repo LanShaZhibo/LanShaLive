5499b4ecebac70199bd81e90b25150883b805cae
Wind <858156085@qq.com>
2016-11-28 11:35:13 +0800

    11

--- app/src/main/java/com/lansha/lanshalive/MainActivity.java
+++ app/src/main/java/com/lansha/lanshalive/MainActivity.java
@@ -2,6 +2,7 @@ package com.lansha.lanshalive;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
+        Log.e("", "onCreate: ");
 
 
     }
